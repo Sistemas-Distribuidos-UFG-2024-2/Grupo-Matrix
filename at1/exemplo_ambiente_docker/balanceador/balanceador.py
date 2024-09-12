8d504cccade18c3a7cab965c1cd5e44bc2f635bf
Pedro Paulo <49731265+pedropaulowski@users.noreply.github.com>
2024-09-12 20:26:42 -0300

    Alterações no código pra executar com docker

---
+++ at1/exemplo_ambiente_docker/balanceador/balanceador.py
@@ -0,0 +1,62 @@
+import socket
+
+servidores = {
+    'servidor1': ('servidor1', 5002),  # Nome do serviço Docker Compose e porta
+    'servidor2': ('servidor2', 5003),
+    'servidor3': ('servidor3', 5004)
+}
+
+carga_servidores = {
+    'servidor1': 0,
+    'servidor2': 0,
+    'servidor3': 0
+}
+
+def balanceador():
+    ultimo_servidor_tentado = ''
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s2:
+        s2.bind(('0.0.0.0', 5000))
+        s2.listen()
+        print("Balanceador iniciado na porta 5000")
+
+        while True:
+            conn, addr = s2.accept()
+            print(f"Conexão recebida de {addr}")
+
+            with conn:
+                data = conn.recv(1024)
+                if not data:
+                    print("Nenhum dado recebido. Encerrando a conexão.")
+                    continue
+
+                count = 1
+
+                # Fazendo loop nos servidores até que acabe os servidores ou encontre um disponível
+                for i in servidores: 
+                    if count == servidores.__len__:
+                        conn.sendall(f'Erro: Nenhum servidor disponível.'.encode('utf-8'))
+                        break
+
+                    servidor = i
+                    servidor_ip, servidor_porta = servidores[servidor]
+                    try:
+                        print(f"Tentando conectar ao {servidor} ({servidor_ip}:{servidor_porta})")
+                        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                            s.connect((servidor_ip, servidor_porta))
+                            s.sendall(data)
+                            resposta = s.recv(1024)
+                            conn.sendall(resposta)
+                        
+                        break
+                    except socket.error as e:
+                        print(f"Erro ao conectar ao {servidor} ({servidor_ip}:{servidor_porta}): {e}")
+                        continue
+                    finally:
+                        count +=1
+                    
+
+
+
+
+if __name__ == "__main__":
+    balanceador()
---
+++ at1/exemplo_ambiente_docker/cliente.py
@@ -0,0 +1,23 @@
+import socket
+import time
+
+def cliente():
+    s2_ip = '127.0.0.1'
+    s2_port = 5000
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        for i in range(5):
+            try:
+                print("Tentando conectar ao servidor...")
+                s.connect((s2_ip, s2_port))
+                break
+            except ConnectionRefusedError:
+                print("Conexão recusada. Tentando novamente...")
+                time.sleep(2) 
+        s.sendall(b'hello')
+        data = s.recv(1024)
+
+    print('Resposta recebida:', data.decode())
+
+if __name__ == "__main__":
+    cliente()
