5ceeded07bcfe9f9f683674e80c5784d25bd7e65
Helloysa <helloysapires.contato@gmail.com>
2024-09-17 16:08:07 -0300

    Update verificador_servicos.py

--- at2/verificador_servicos.py
+++ at2/verificador_servicos.py
@@ -2,45 +2,45 @@ import socket
 import threading
 import time
 
-servidores_ativos = [] # Lista dos servidores ativos
+servidores_ativos = []  # Lista dos servidores ativos 
 
 def registrar_servidor():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as verificador:
-        verificador.bind(('127.0.0.1', 6000)) # Porta para servidores
+        verificador.bind(('127.0.0.1', 6000))  # Porta para servidores
         verificador.listen()
         while True:
             conn, addr = verificador.accept()
             with conn:
-                data = conn.recv(1024)
-                if data.decode() == 'ativo': # Recebe a mensagem do servidor ativo
-                    print(f"Servidor {addr[0]}:{addr[1]} registrou como ativo.")
-                    servidores_ativos.append(addr[0]) # Adiciona o servidor na lista de servidores ativos
-
-def verificar_servidor(ip):
+                data = conn.recv(1024).decode()
+                ip, porta = data.split(':')  # Recebe diretamente o IP e porta
+                servidores_ativos.append((ip, int(porta)))  # Adiciona o servidor (IP, porta) à lista de servidores ativos
+                print(f"Servidor {ip}:{porta} registrou como ativo.")
+                
+def verificar_servidor(ip, porta):
     try:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.settimeout(2)  #se o socket não conseguir estabelecer uma conexão, uma exceção socket.timeout será lançada
-            s.connect((ip, 5002)) 
+            s.settimeout(2)  # Se o socket não conseguir estabelecer uma conexão, uma exceção socket.timeout será lançada
+            s.connect((ip, porta))  # Usa a porta recebida do servidor
             return True
     except socket.error:
         return False
 
 def atender_balanceador():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as verificador:
-        verificador.bind(('127.0.0.1', 6001)) # Porta para balanceador
+        verificador.bind(('127.0.0.1', 6001))  # Porta para balanceador
         verificador.listen()
         while True:
             conn, addr = verificador.accept()
             with conn:
                 if servidores_ativos:
-                    for ip in servidores_ativos: 
-                        if verificar_servidor(ip): # Chama a função para verificar se está realemente ativo
-                            conn.sendall(ip.encode()) # Se estiver ativo, envia para o balanceador
-                            print(f"Verificador enviou o IP {ip} ao balanceador.")
+                    for ip, porta in servidores_ativos: 
+                        if verificar_servidor(ip, porta):  # Chama a função para verificar se está realmente ativo
+                            conn.sendall(f'{ip}:{porta}'.encode())  # Se estiver ativo, envia IP e porta para o balanceador
+                            print(f"Verificador de serviços enviou o IP {ip}:{porta} ao balanceador.")
                             break
                         else:
-                            servidores_ativos.remove(ip) # Se não estiver ativo, remove o ip da lista
-                            print(f"Servidor {ip} removido da lista.")
+                            servidores_ativos.remove((ip, porta))  # Se não estiver ativo, remove o IP e porta da lista
+                            print(f"Servidor {ip}:{porta} removido da lista.")
                 else:
                     conn.sendall(b'Nenhum servidor ativo.')
 
