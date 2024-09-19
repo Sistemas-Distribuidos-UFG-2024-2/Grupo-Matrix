23ae94d8c5b38604e55bcc0f5d18d1b91a7ce4cb
Helloysa <helloysapires.contato@gmail.com>
2024-09-19 14:28:32 -0300

    Update servidor1.py

--- at2/servidor1.py
+++ at2/servidor1.py
@@ -1,26 +1,37 @@
 import socket
+import threading
+import time
 
 ip_servidor = '127.0.0.1'
-porta_servidor = 5002
+porta_servidor = 5002  
 
 def notificar_verificador_de_servicos():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as servidor:
-        servidor.connect(('127.0.0.1', 6000))  
-        mensagem = f'{ip_servidor}:{porta_servidor}' 
-        servidor.sendall(mensagem.encode())  
+    while True:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as servidor:
+            try:
+                servidor.connect(('127.0.0.1', 6000))  
+                mensagem = f'{ip_servidor}:{porta_servidor}'  # Envia IP e porta para o verificador
+                servidor.sendall(mensagem.encode())  
+            except ConnectionRefusedError:
+                print("Não foi possível conectar ao verificador de serviços.")
+            time.sleep(5)  # Notifica o verificador a cada 5 segundos
 
 def servidor():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((ip_servidor, porta_servidor)) 
         s.listen()
-        print(f"Servidor aguardando conexão em {ip_servidor}:{porta_servidor}...")
-        conn, addr = s.accept()  
-        with conn:
-            data = conn.recv(1024)  
-            print(f"Servidor recebeu '{data.decode()}' de {addr[0]}:{addr[1]}")
-            conn.sendall(b'world!')  
-            print(f"Servidor enviou 'world!' para {addr[0]}:{addr[1]}")
+        print(f"Servidor aguardando conexões em {ip_servidor}:{porta_servidor}...")
+        while True:
+            conn, addr = s.accept() 
+            threading.Thread(target=conectar_balanceador, args=(conn, addr)).start()
 
+def conectar_balanceador(conn, addr):
+    with conn:
+        data = conn.recv(1024)  
+        if data:
+            print(f"Servidor {porta_servidor} recebeu '{data.decode()}' de {addr[0]}:{addr[1]}")
+            conn.sendall(b'world!')  # Envia resposta ao cliente
+            print(f"Servidor {porta_servidor} enviou 'world!' para {addr[0]}:{addr[1]}")
 
-notificar_verificador_de_servicos()
+threading.Thread(target=notificar_verificador_de_servicos).start()
 servidor()
