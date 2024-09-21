97ca4875f0efb277d6ea94087f142c3448dd9a25
Helloysa <helloysapires.contato@gmail.com>
2024-09-20 18:08:58 -0600

    Create servidor3.py

---
+++ at3/servidor3.py
@@ -0,0 +1,34 @@
+import socket
+import threading
+
+ip_servidor = '127.0.0.1'
+porta_servidor = 5004
+
+def notificar_verificador_de_servicos():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as servidor:
+        try:
+            servidor.connect(('127.0.0.1', 6000))  
+            mensagem = f'{ip_servidor}:{porta_servidor}'  # Envia IP e porta para o verificador
+            servidor.sendall(mensagem.encode())  
+        except ConnectionRefusedError:
+            print("Não foi possível conectar ao verificador de serviços.")
+
+def servidor():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((ip_servidor, porta_servidor)) 
+        s.listen()
+        print(f"Servidor aguardando conexões em {ip_servidor}:{porta_servidor}...")
+        while True:
+            conn, addr = s.accept() 
+            threading.Thread(target=conectar_balanceador, args=(conn, addr)).start()
+
+def conectar_balanceador(conn, addr):
+    with conn:
+        data = conn.recv(1024)  
+        if data:
+            print(f"Servidor {porta_servidor} recebeu '{data.decode()}' de {addr[0]}:{addr[1]}")
+            conn.sendall(b'world!')  # Envia resposta ao cliente
+            print(f"Servidor {porta_servidor} enviou 'world!' para {addr[0]}:{addr[1]}")
+
+notificar_verificador_de_servicos()
+servidor()
