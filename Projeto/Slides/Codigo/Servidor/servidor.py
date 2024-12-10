b746fa741e3033970fb3c2c68fd90298d5570fd0
Nicolas Maulaiz <104225083+NicolasMlz@users.noreply.github.com>
2024-12-10 19:35:14 -0300

    Update servidor.py

--- Projeto/Slides/Codigo/Servidor/servidor.py
+++ Projeto/Slides/Codigo/Servidor/servidor.py
@@ -34,7 +34,7 @@ def criar_tabela(engine):
     # Definição do esquema da tabela `noticia`
     noticia_table = Table(
         "noticia", metadata,
-        Column("id", Integer, primary_key=True, autoincrement=False),
+        Column("id", Integer, primary_key=True, autoincrement=True),
         Column("manchete", Text, nullable=False),
         Column("subtitulo", Text),
         Column("texto", Text, nullable=False),
